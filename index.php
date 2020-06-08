c1cb520e6c1e6aeda010a018aa16b47383acd994
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 16:14:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    function testHuman($age, $gender){
+        if ($gender == 'Homme' && $age >= 18 ){
+            echo 'Vous êtes un homme et vous êtes majeur';
+        }else if ($gender == 'Homme' && $age < 18){
+            echo'Vous êtes un homme et vous êtes mineur';
+        }else if ($gender == 'Femme' && $age >= 18){
+            echo 'Vous êtes une femme et vous êtes majeure';
+        }else if ($gender == 'Femme' && $age < 18){
+            echo 'Vous êtes une femme et vous êtes mineur';
+        }
+    } 
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>partie 4 exercice 7</title>
+    </head>
+    <body>
+        <p>
+            <?php 
+                testHuman(25, 'Femme');
+            ?>
+        </p>
+    </body>
+</html>
