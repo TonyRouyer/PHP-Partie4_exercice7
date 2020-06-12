5c245fb95ebbe2b76dc930b2f63a75605c0704c2
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-12 14:36:09 +0200

    correction

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
     function testHuman($age, $gender){
+        $message = 'une valeur est incorrecte';
         if ($gender == 'Homme' && $age >= 18 ){
-            echo 'Vous êtes un homme et vous êtes majeur';
+            $message = 'Vous êtes un homme et vous êtes majeur';
         }else if ($gender == 'Homme' && $age < 18){
-            echo'Vous êtes un homme et vous êtes mineur';
+            $message ='Vous êtes un homme et vous êtes mineur';
         }else if ($gender == 'Femme' && $age >= 18){
-            echo 'Vous êtes une femme et vous êtes majeure';
+            $message = 'Vous êtes une femme et vous êtes majeure';
         }else if ($gender == 'Femme' && $age < 18){
-            echo 'Vous êtes une femme et vous êtes mineur';
+            $message = 'Vous êtes une femme et vous êtes mineur';
         }
     } 
 ?>
